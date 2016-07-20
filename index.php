ece585e55ee6926451a3bfa5904cf46fc0c92ecb
habtsr <hebtsr@gmail.com>
2016-07-20 22:00:07 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+
+	/*
+	*
+	*	@Project	-> PHP , Class dealing with files ;
+	*
+	*	@Author		-> AmineDine ;
+	*
+	*	@Mail		-> Minousta@outlook.fr ;
+	*
+	*	@Facebook	-> http://www.facebook.com/aminebtrs ;
+	*
+	*/
+
+	/*
+	*
+	*	Inlude Class ;
+	*
+	*/
+
+	include ( './class.dealingfiles.php' ) ;
+	
+	/*
+	*
+	*	Run the Class ;
+	*
+	*/
+
+	$Run = new Load () ;
+	
+	/*
+	*
+	*	Object add file ;
+	*
+	*/
+	
+	//$Run->Files->AddFile ( 'test.txt' ) ;
+	
+	/*
+	*
+	*	Object read file ;
+	*
+	*	Parameter second use ALL or numbers ;
+	*
+	*/
+	
+	//echo $Run->Files->ReadFile ( 'test.txt' , 'ALL' ) ;
+	
+	/*
+	*
+	*	Object write in file ;
+	*
+	*/
+
+	//$Run->Files->WriteFile ( 'test.txt' , 'Bentsair Boutaleb Amine' ) ;
+	
+	/*
+	*
+	*	Object edit file ;
+	*
+	*/
+ 
+	//$Run->Files->EditFile ( 'test.txt' , 'Amine' , 'Mohamed' ) ;
+	
+	/*
+	*
+	*	Object remove file ;
+	*
+	*/
+	
+	//$Run->Files->RemoveFile ( 'test.txt' ) ;
+
+?>
